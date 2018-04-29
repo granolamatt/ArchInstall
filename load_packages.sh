13a38f86ce3c3c12dd07172a32310338d594a9c0
Granolamatt Code Base <granolamatt@gmail.com>
2018-04-29 16:57:52 -0600

    Update load_packages.sh
    Fixed the load_packages script

--- load_packages.sh
+++ load_packages.sh
@@ -1,6 +1,2 @@
-find / -regextype posix-extended -regex "/(sys|srv|proc)|.*/\.ccache/.*" -prune -o -type f \
--exec bash -c 'file "{}" | grep -E "(32|64)-bit"' \; | \
-awk -F: '{print $1}' | \
-while read -r bin; \
-do pacman -Qo "$bin" &>/dev/null || echo "$bin"; \
-done
+#!/bin/bash
+xargs -a Packages pacman -S --noconfirm --needed
